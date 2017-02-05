a2a34d9772aca271bd255a9abd435084c0f0c49d
Chris Chan <chanis@yahoo-inc.com>
2017-02-04 22:17:52 -0800

    Added click handler for each item in the recycler view.

--- app/src/main/java/com/codepath/newssearch/adapter/ArticleAdapter.java
+++ app/src/main/java/com/codepath/newssearch/adapter/ArticleAdapter.java
@@ -2,6 +2,7 @@ package com.codepath.newssearch.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,7 +34,18 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
 
         LinearLayout layout = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.article_item, parent, false);
 
-        ViewHolder holder = new ViewHolder(layout);
+        ViewHolder holder = new ViewHolder(layout, new ArticleAdapter.ViewHolder.IViewHolderClicks() {
+            public void onClick(View caller) {
+                Log.d("VEGETABLES", "Poh-tah-tos");
+            };
+            public void onClick(ImageView caller) {
+                Log.d("IMAGE", "Poh-tah-tos1");
+            };
+            public void onClick(TextView caller) {
+                Log.d("TEXT", "Poh-tah-tos2");
+            };
+
+        });
         return holder;
     }
 
@@ -58,15 +70,37 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         return articles.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView headline;
         ImageView thumbnail;
+        IViewHolderClicks listener;
 
-        public ViewHolder(View itemView) {
+        public ViewHolder(View itemView, IViewHolderClicks listener) {
             super(itemView);
+            this.listener = listener;
 
             this.headline = (TextView) itemView.findViewById(R.id.tvHeadline);
+            this.headline.setOnClickListener(this);
             this.thumbnail = (ImageView) itemView.findViewById(R.id.ivThumbnail);
+            this.thumbnail.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v instanceof ImageView) {
+                listener.onClick((ImageView) v);
+            } else if (v instanceof TextView) {
+                listener.onClick((TextView) v);
+            }
+            else {
+                listener.onClick(v);
+            }
+        }
+
+        public static interface IViewHolderClicks {
+            public void onClick(TextView caller);
+            public void onClick(ImageView caller);
+            public void onClick(View caller);
         }
     }
 }
